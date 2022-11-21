76c190be637d19c278359de687bddcadaeec431e
chienkt <kennyjoe1311@gmail.com>
2022-11-22 01:16:47 +0700

    wip sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -3,9 +3,21 @@ if (isProduction && 'serviceWorker' in navigator) {
       navigator.serviceWorker
          .register('service-worker.js', { scope: './' })
          .then((registration) => {
-			registration.onupdatefound = (evt) => {
-				console.log("Service Worker on update found event: ", evt);
-			}
+            serviceWorkerRegistration.pushManager.subscribe().then(
+               (pushSubscription) => {
+                  console.log('Fired a push event: ', pushSubscription.endpoint);
+                  // The push subscription details needed by the application
+                  // server are now available, and can be sent to it using,
+                  // for example, an XMLHttpRequest.
+               },
+               (error) => {
+                  console.error(error);
+               }
+            );
+            registration.onupdatefound = () => {
+               const installingWorker = registration.installing;
+               console.log('A new service worker is being installed:', installingWorker);
+            };
             console.log('Service Worker registered: ', registration);
          })
          .catch((registrationError) => {
