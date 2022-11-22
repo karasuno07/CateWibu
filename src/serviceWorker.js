a6dce0b7e72a07170ee7c55797cd404518ab4508
chienkt <kennyjoe1311@gmail.com>
2022-11-22 12:08:07 +0700

    ws

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -3,6 +3,17 @@ if (isProduction && 'serviceWorker' in navigator) {
       navigator.serviceWorker
          .register('service-worker.js', { scope: './' })
          .then((registration) => {
+            // if ('PushManager' in window && registration.active) {
+            //    registration.pushManager.subscribe({ userVisibleOnly: true }).then(
+            //       (pushSubscription) => {
+            //          console.log('Fired a push event: ', pushSubscription.endpoint);
+            //       },
+            //       (error) => {
+            //          console.error(error);
+            //       }
+            //    );
+            // }
+
             registration.addEventListener('updatefound', () => {
                const installingWorker = registration.installing;
                console.log('A new service worker is being installed:', installingWorker);
