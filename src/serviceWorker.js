27bec18f7382ef8e3c6ee6095d3fcd4e01204bd3
chienkt <kennyjoe1311@gmail.com>
2022-11-22 11:20:46 +0700

    wip sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -3,16 +3,16 @@ if (isProduction && 'serviceWorker' in navigator) {
       navigator.serviceWorker
          .register('service-worker.js', { scope: './' })
          .then((registration) => {
-            if (registration.active) {
-               registration.pushManager.subscribe().then(
-                  (pushSubscription) => {
-                     console.log('Fired a push event: ', pushSubscription.endpoint);
-                  },
-                  (error) => {
-                     console.error(error);
-                  }
-               );
-            }
+            // if (registration.active) {
+            //    registration.pushManager.subscribe().then(
+            //       (pushSubscription) => {
+            //          console.log('Fired a push event: ', pushSubscription.endpoint);
+            //       },
+            //       (error) => {
+            //          console.error(error);
+            //       }
+            //    );
+            // }
 
             registration.addEventListener('updatefound', () => {
                const installingWorker = registration.installing;
